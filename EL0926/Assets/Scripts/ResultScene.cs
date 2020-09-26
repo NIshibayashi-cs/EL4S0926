101b1623ad8e972a962cd62203df93e7c3b4c46e
ｃ_soda <csoda.6436@gmail.com>
2020-09-26 11:09:23 +0900

    Resulesceneのスクリプト作成

---
+++ EL0926/Assets/Scripts/ResultScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultScene : MonoBehaviour
+{
+    public GameObject clear_root;
+    public GameObject fail_root;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var cleard = PlayerPrefs.GetInt("cleard", 0) > 1;
+        clear_root.SetActive(cleard);
+        fail_root.SetActive(!cleard);
+    }
+
+
+}
