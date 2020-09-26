64d3d19376dd077eba17112ac2d81a7c8abb928b
sekine_kenta <sekken_oo@yahoo.co.jp>
2020-09-26 12:09:06 +0900

    Title作成

---
+++ EL0926/Assets/Images/Titleimg/Thanayama.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thanayama : MonoBehaviour
+{
+    public int dist;
+    int counter = 0;
+    float plus = 0.03f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 p = new Vector2(0,plus);
+        transform.Translate(p);
+        counter++;
+        if (counter == dist)
+        {
+            counter = 0;
+            plus *= -1;
+        }
+
+    }
+}
