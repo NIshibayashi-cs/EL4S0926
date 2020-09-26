263091054379eed236f43ec2a2d5bb4f19066c65
NIshibayashi-cs <stardustfactory.beca@gmail.com>
2020-09-26 14:18:00 +0900

    先生クリック判定処理仮組み

---
+++ EL0926/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit.collider == null) return;
+            var script = hit.collider.gameObject.GetComponent<Sensei>();
+            if (script == null) return;
+            Debug.Log(script.ToString());
+        }
+    }
+}
